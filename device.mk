c2a7fa0d1b75f5fda8933b5038a75c2fca518321
sjllls <2012ji123@gmail.com>
2020-03-22 18:51:16 -0500

    Add audio configs from stock

--- device.mk
+++ device.mk
@@ -25,3 +25,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Inherit from tone-common
 $(call inherit-product, device/sony/tone-common/platform.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
